faf8d5eeea394f798a8f5ddbdf4ac3ced2412824
Fatemeh Anami <97031840+FatemehAnami@users.noreply.github.com>
2023-06-17 11:34:59 +0530

    Update data_dump.py

--- data_dump.py
+++ data_dump.py
@@ -4,15 +4,12 @@ import json
 import os
 from APS_Sensor import config
 
-# Provide the mongodb localhost url to connect python to mongodb.
-#client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-
 DATABASE_NAME = "APS_Sensor_Database"
 COLLECTION_NAME = "APS_Sensor_Collection"
 if __name__ == "__main__":
     df= pd.read_csv('/config/workspace/Data/aps_failure_training_set.csv')
     print('Shape of data is: ', df.shape)
-        # change data to json to dump into mongodb
+    # change data to json to dump into mongodb
     df.reset_index(drop = True , inplace = True)
     json_records = list(json.loads(df.T.to_json()).values())
     print(json_records[0])
